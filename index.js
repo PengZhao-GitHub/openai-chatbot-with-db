8455fbae37dd41dc85ba80cdc784d723c1cd93ef
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-02 13:39:24 +0900

    chat-bot-with-db

---
+++ index.js
@@ -0,0 +1,121 @@
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, push, get, remove } from 'firebase/database'
+
+import { Configuration, OpenAIApi } from 'openai'
+import { process } from './env'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+const appSettings = {
+    databaseURL: 'https://knowitall-openai-763d9-default-rtdb.asia-southeast1.firebasedatabase.app/'
+}
+
+const app = initializeApp(appSettings)
+
+const database = getDatabase(app)
+
+const conversationInDb = ref(database)
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+
+const instructionObj = {
+    role: 'system',
+    content: 'You are an assistant that gives very short answers.'
+}
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input')
+    push(conversationInDb, {
+        role: 'user',
+        content: userInput.value
+    })
+    fetchReply()
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+    userInput.value = ''
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+})
+
+function fetchReply() {
+    get(conversationInDb).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            const conversationArr = Object.values(snapshot.val())
+            conversationArr.unshift(instructionObj)
+            const response = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: conversationArr,
+                presence_penalty: 0,
+                frequency_penalty: 0.3
+            })
+            /*
+            Challenge:
+                1. Add the completion to the database.
+                2. Ask the chatbot something to check it's working.
+            */
+            push(conversationInDb, response.data.choices[0].message)
+            renderTypewriterText(response.data.choices[0].message.content)
+        }
+        else {
+            console.log('No data available')
+        }
+
+    })
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i - 1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
+
+document.getElementById('clear-btn').addEventListener('click', () => {
+    remove(conversationInDb)
+    chatbotConversation.innerHTML = '<div class="speech speech-ai">How can I help you?</div>'
+
+})
+
+/*
+Challenge:
+    1. Create a function called renderConversationFromDb which 
+       will render any existing conversation in the database.
+    2. This function should be called when the app loads.
+*/ 
+
+function renderConversationFromDb() {
+    get(conversationInDb).then(async (snapshot) => {
+        if(snapshot.exists()) {
+            Object.values(snapshot.val()).forEach(dbObj => {
+                const newSpeechBubble = document.createElement('div')
+                newSpeechBubble.classList.add(
+                    'speech',
+                    `speech-${dbObj.role === 'user' ? 'human' : 'ai'}`
+                )
+                chatbotConversation.appendChild(newSpeechBubble)
+                newSpeechBubble.textContent = dbObj.content
+            })
+
+            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+        }
+    })
+}
+
+renderConversationFromDb()
+
+
